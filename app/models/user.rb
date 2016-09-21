6d38f8c0ca60db68452a7428b26ef5f3cbe1c729
macarci <macarci@gmail.com>
2016-09-21 12:48:05 -0400

    User scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User
   include Mongoid::Document
+  Cenit::MultiTenancy::UserScope
   include Mongoid::Attributes::Dynamic
 
   def confirmed?
     attributes[:confirmed_at].present?
   end
 end
-
-Cenit::MultiTenancy.user_model User
