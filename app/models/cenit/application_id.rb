53af9261010d131121f11c335899cb6bb8236b22
macarci <macarci@gmail.com>
2016-09-28 09:17:36 -0400

    Prevent no associated tenant

--- app/models/cenit/application_id.rb
+++ app/models/cenit/application_id.rb
@@ -33,7 +33,7 @@ module Cenit
     end
 
     def app
-      @app ||= Cenit::Oauth.app_model.with(tenant).where(application_id: self).first
+      @app ||= tenant && Cenit::Oauth.app_model.with(tenant).where(application_id: self).first
     end
 
     def name
