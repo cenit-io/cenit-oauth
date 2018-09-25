b0c07192534c4b6bc945bb0f53f835921d92c3fc
Maikel Arcia Carrazana <macarci@gmail.com>
2018-09-25 17:38:21 -0300

    Fixing App IDs validation

--- app/models/cenit/application_id.rb
+++ app/models/cenit/application_id.rb
@@ -13,15 +13,21 @@ module Cenit
     validates_length_of :slug, maximum: 255
     validates_format_of :slug, with: /\A([a-z]([_-])?)*\Z/
 
-    before_save do
+    validate do
       [:oauth_name, :slug].each do |field|
         if self[field].blank?
           remove_attribute(field)
         else
           self[field] = self[field].strip
-          errors.add(field, 'is already taken') if self.class.where(field => self[field]).exists?
+          if self.class.where(:id.ne => id).and(field => self[field]).exists?
+            errors.add(field, 'is already taken')
+          end
         end
       end
+      errors.blank?
+    end
+
+    before_save do
       self.tenant ||= Cenit::MultiTenancy.tenant_model.current_tenant
       self.identifier ||= (id.to_s + Token.friendly(60))
       if @redirect_uris
